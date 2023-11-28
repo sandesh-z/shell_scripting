57dd840da4f3cf3131bb4c2529a5d02819cc153f
Sandace <gsandace@gmail.com>
2023-11-28 20:17:43 +0545

    first script

---
+++ first.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This is a comment!
+# First line defines file is to be executed by /bin/sh and
+# "# and !", is a special directive which Unix treats specially.
+# Give executive permission before running script like chmod 755 first.sh
+# Run with ./filename.sh
+echo Hello World  
