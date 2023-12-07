21fbc8a177647ba78624d0c63c9cfa78c032f8fc
Sandace <gsandace@gmail.com>
2023-12-07 20:26:18 +0545

    escape characters in shell

---
+++ escape_characters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+X=5
+#A quote is ", backslash is \, backtick is `.
+#A few spaces are    and dollar is $. $X is 5.
+
+
+echo "A quote is \", backslash is \\, backtick is \`."
+ 
+echo "A few spaces are    and dollar is \$. \$X is ${X}."
+ 
