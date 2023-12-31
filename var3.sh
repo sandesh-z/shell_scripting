3d74cda375fca7ee1245650f182229f4112b8c4c
sandesh <gsandace@gmail.com>
2023-12-31 13:56:33 +0545

    defining default variables in shell

---
+++ var3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo -en "What is your name [ `whoami` ] "
+read myname
+# if [ -z "$myname" ]; then
+#   myname=`whoami`
+# fi
+#here default value would would be whoami which is defined with :-'whoami'
+echo "Your name is : ${myname:-`whoami`}"
+
+#here default value would be John Doe
+echo "Your name is : ${myname:-John Doe}"
+
+#another way to define default variable
+echo "Your name is : ${myname:= Also John Doe}"
